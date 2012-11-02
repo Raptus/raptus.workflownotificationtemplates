60646181778ee3065e49e74d22ba8cf16689fe88
Jonas Wagner <jwagner@raptus.com>
2012-11-02 13:53:08 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0b1'
 setup(name='raptus.workflownotificationtemplates',
       version=version,
       description="Provides a configlet to write notification templates ttw",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='raptus.workflownotificationtemplates',
       keywords='plone notification',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.workflownotificationtemplates',
+      url='https://github.com/Raptus/raptus.workflownotificationtemplates',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus'],
